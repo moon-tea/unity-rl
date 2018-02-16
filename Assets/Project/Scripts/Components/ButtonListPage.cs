da18762127a7d9dbf6a4b6f01fde2a7c3a91dde5
moon-tea <monte.nichols.ii@gmail.com>
2018-02-16 12:56:56 -0600

    changed the name of library as it is reserved, I think.

---
+++ Assets/Project/Scripts/Components/ButtonListPage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public class ButtonListPage
+{
+    public ButtonListPage(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+        description = new Description(
+            text: "ButtonLists render stacked buttons also known as menus",
+            x: 20,
+            y: 0,
+            //height: 4,
+            width: 20,
+            layer: 1
+        );
+        buttonLists = new ButtonList[] {
+            new ButtonList(
+                new Button[] {
+                    new Button(
+                        text: "Button 0"
+                    ),
+                    new Button(
+                        text: "Button 1"
+                    ),
+                    new Button(
+                        text: "Button 2"
+                    )
+                },
+                x: x,
+                y: description.height + y + 1,
+                layer: 1
+           )
+        };
+    }
+
+    public int x;
+    public int y;
+    public ButtonList[] buttonLists;
+    public Description description;
+
+    public void render()
+    {
+        for (var i = 0; i < buttonLists.Length; i++)
+        {
+            if (buttonLists[i] != null)
+            {
+                buttonLists[i].render();
+            }
+        }
+
+        if (description != null)
+        {
+            description.render();
+        }
+    }
+}
---
+++ Assets/Project/Scripts/Components/ButtonPage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class ButtonPage
+{
+	public ButtonPage(int x, int y)
+	{
+        this.x = x;
+        this.y = y;
+        description = new Description(
+            text: "Buttons have a hover state and can deploy callback methods",
+            x: 20,
+            y: 0,
+            //height: 4,
+            width: 20,
+            layer: 1
+        );
+        buttons = new Button[] {
+            new Button(
+               text: "Example Button",
+               x: 20,
+               y: description.y + description.height + 1,
+               layer: 1
+            )
+        };
+	}   
+
+    public int x;
+    public int y;
+    public Button[] buttons;
+    public Description description;
+
+    public void render()
+    {
+        for (var i = 0; i < buttons.Length; i++)
+        {
+            if (buttons[i] != null)
+            {
+                buttons[i].render();
+            }
+        }
+
+        if (description != null)
+        {
+            description.render();
+        }
+    }
+}
